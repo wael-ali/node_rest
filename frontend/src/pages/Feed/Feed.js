b42dcfeac5daa1ccfa3c4fa73720982dca3d31fe
wal <wael.ali@elbformat.de>
2020-10-15 22:26:32 +0200

    409-- Identifying realtime potintial

--- frontend/src/pages/Feed/Feed.js
+++ frontend/src/pages/Feed/Feed.js
@@ -42,7 +42,19 @@ class Feed extends Component {
     this.loadPosts();
     openSocket('http://localhost:8080');
   }
-
+  addPost = post => {
+     this.setState(prevState => {
+       const updatedPosts = [...prevState.posts];
+       if (prevState.postPage === 1){
+         updatedPosts.pop();
+         updatedPosts.unshift(post);
+       }
+       return {
+         posts: updatedPosts,
+         totalPosts: prevState.totalPosts + 1
+       }
+     });
+  };
   loadPosts = direction => {
     if (direction) {
       this.setState({ postsLoading: true, posts: [] });
