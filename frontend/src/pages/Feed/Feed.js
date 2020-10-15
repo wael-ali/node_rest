eba1b3ecb984a8a978180eb25b460805acfb587d
wal <wael.ali@elbformat.de>
2020-10-15 23:28:54 +0200

    413-- updating posts in all connected clients

--- frontend/src/pages/Feed/Feed.js
+++ frontend/src/pages/Feed/Feed.js
@@ -42,7 +42,12 @@ class Feed extends Component {
     this.loadPosts();
     const socket = openSocket('http://localhost:8080');
     socket.on('posts', data => {
-      this.addPost(data.post);
+      if (data.action === 'create'){
+        this.addPost(data.post);
+      }
+      if (data.action === 'update'){
+        this.updatePost(data.post);
+      }
     });
   }
   addPost = post => {
@@ -55,6 +60,19 @@ class Feed extends Component {
        return {
          posts: updatedPosts,
          totalPosts: prevState.totalPosts + 1
+       }
+     });
+  };
+  updatePost = post => {
+     this.setState(prevState => {
+       const updatedPosts = [...prevState.posts];
+       const updatedPostIndex = updatedPosts.findIndex(p => p._id === post._id);
+       if (updatedPostIndex > -1){
+         // updatedPosts.pop();
+         updatedPosts[updatedPostIndex] = post;
+       }
+       return {
+         posts: updatedPosts,
        }
      });
   };
@@ -178,15 +196,7 @@ class Feed extends Component {
           createdAt: resData.post.createdAt
         };
         this.setState(prevState => {
-          let updatedPosts = [...prevState.posts];
-          if (prevState.editPost) {
-            const postIndex = prevState.posts.findIndex(
-              p => p._id === prevState.editPost._id
-            );
-            updatedPosts[postIndex] = post;
-          }
           return {
-            posts: updatedPosts,
             isEditing: false,
             editPost: null,
             editLoading: false
