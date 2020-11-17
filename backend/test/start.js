2262e4494e7c0a0c92575723224c1017a7f028ad
wael-ali <waelali595@gmail.ocm>
2020-11-17 09:55:43 +0100

    --466-- Testing auth middleware

--- backend/test/start.js
+++ backend/test/start.js
@@ -1,15 +1,15 @@
-const expect = require('chai').expect
-
-it('should Add numbers correctly', function () {
-    const num1 = 2;
-    const num2 = 3;
-
-    expect(num1 + num2).to.equal(5);
-})
-
-it('should not give the number 6', function () {
-    const num1 = 3;
-    const num2 = 3;
-
-    expect(num1 + num2).to.equal(5);
-})
+// const expect = require('chai').expect
+//
+// it('should Add numbers correctly', function () {
+//     const num1 = 2;
+//     const num2 = 3;
+//
+//     expect(num1 + num2).to.equal(5);
+// })
+//
+// it('should not give the number 6', function () {
+//     const num1 = 3;
+//     const num2 = 3;
+//
+//     expect(num1 + num2).to.equal(5);
+// })
