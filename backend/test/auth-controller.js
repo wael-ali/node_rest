2932167630c0fe881ba45c33ba4513f548b1ab5f
wael-ali <waelali595@gmail.ocm>
2020-11-20 13:30:01 +0100

    --475-- Hooks

--- backend/test/auth-controller.js
+++ backend/test/auth-controller.js
@@ -6,6 +6,50 @@ const User = require('../models/user');
 const AuthController = require('../controllers/auth');
 
 describe('Auth Controller - Login', function () {
+    // Runs once before the tests begin
+    before(function (done) {
+        const user_id = '5f91705e0d48d458084f2bb2';
+        const user_status = 'newly created!!';
+        mongose.connect(
+                'mongodb+srv://node_toturial_1:WNct0GPpfk1rQfrv@cluster0.q4mpc.mongodb.net/node_test_2?retryWrites=true&w=majority'
+            )
+            .then(result => {
+                const user = new User({
+                    email: 'test@test.com',
+                    password: 'foobar',
+                    name: 'John Doe',
+                    status: user_status,
+                    posts: [],
+                    _id: user_id
+                });
+                return user.save();
+            })
+            .then(() => {
+                done();
+            })
+            .catch(err => console.log(err))
+        ;
+    });
+    // Runs once after the tests finish.
+    after(function(done) {
+        User.deleteMany({}) // Delete all users
+            .then(() => {
+                return mongose.disconnect();
+            })
+            .then(() => {
+                done();
+            })
+        ;
+    });
+    // Runs before every test
+    beforeEach(function(){
+        console.log('Before test ...');
+    });
+    // Runs After every test
+    beforeEach(function(){
+        console.log('After test ...');
+    });
+    // Hier goes the tests
     it('should throw an error if accessing the database fails', function (done) {
         sinon.stub(User, 'findOne'); 
         User.findOne.throws();
@@ -28,51 +72,25 @@ describe('Auth Controller - Login', function () {
     it('should send aresponse with a valid user status for an existing user', function (done) {
         const user_id = '5f91705e0d48d458084f2bb2';
         const user_status = 'newly created!!';
-        mongose
-        .connect(
-                'mongodb+srv://node_toturial_1:WNct0GPpfk1rQfrv@cluster0.q4mpc.mongodb.net/node_test_2?retryWrites=true&w=majority'
-            )
-            .then(result => {
-                const user = new User({
-                    email: 'test@test.com',
-                    password: 'foobar',
-                    name: 'John Doe',
-                    status: user_status,
-                    posts: [],
-                    _id: user_id
-                });
-                return user.save();
-                // done();
-            })
+        
+        const req = { userId: user_id };
+        const res = { 
+            statusCode: 200,
+            userStatus: null,
+            status: function (code) {
+                this.statusCode = code;
+                return this;
+            },
+            json: function (data) {
+                this.userStatus = data.status
+            }
+        };
+        AuthController.getUserStatus(req, res, () => {})
             .then(() => {
-                const req = { userId: user_id };
-                const res = { 
-                    statusCode: 200,
-                    userStatus: null,
-                    status: function (code) {
-                        this.statusCode = code;
-                        return this;
-                    },
-                    json: function (data) {
-                        this.userStatus = data.status
-                    }
-                };
-                AuthController.getUserStatus(req, res, () => {})
-                    .then(() => {
-                        expect(res.statusCode).to.be.equal(200);
-                        expect(res.userStatus).to.be.equal(user_status);
-                        User.deleteMany({}) // Delete all users
-                            .then(() => {
-                                return mongose.disconnect();
-                            })
-                            .then(() => {
-                                done();
-                            })
-                        ;
-                    })
-                ;
+                expect(res.statusCode).to.be.equal(200);
+                expect(res.userStatus).to.be.equal(user_status);
+                done()
             })
-            .catch(err => console.log(err))
         ;
     });
 });
